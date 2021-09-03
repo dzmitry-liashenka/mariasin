73e7c75fa1dd694dd21debd5a3ea12c41249af50
liashenka <dzmitry_liashenka@yahoo.com>
2021-09-04 00:05:35 +0200

    version für deploy 1

--- src/main/java/de/mariasin/shop/boundary/CustomerListBean.java
+++ src/main/java/de/mariasin/shop/boundary/CustomerListBean.java
@@ -1,16 +1,13 @@
 package de.mariasin.shop.boundary;
 
 import java.io.Serializable;
-import java.text.DecimalFormat;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 
-import de.mariasin.shop.control.CustomerService;
-import de.mariasin.shop.entity.Customer;
+import de.mariasin.shop.entity.User;
 
 @Named
 @ViewScoped
@@ -18,60 +15,15 @@ public class CustomerListBean implements Serializable {
 
 	private static final long serialVersionUID = 4773746274170179581L;
 
-	private List<Customer> customers;
-	private List<Customer> filteredCustomerList;
-	private List<Customer> selectedCustomerList;
+	private List<User> users;
 
-	@Inject
-	private CustomerService customerService;
+
+//	@Inject
+//	private ImageService imageService;
 
 	@PostConstruct
 	public void init() {
-		customers = customerService.getCustomers();
+		
 	}
 
-	public String getTotalRevenue() {
-		if (this.customers == null) {
-			return "0";
-		}
-
-		Long totalRevenue = customers.stream().mapToLong(Customer::getBilledRevenue).sum();
-		return new DecimalFormat("###,###.###").format(totalRevenue);
-	}
-
-	public void deleteCustomers() {
-		for (Customer customer : selectedCustomerList) {
-			this.customerService.deleteCustomer(customer);
-
-			if (filteredCustomerList != null) {
-				this.filteredCustomerList.remove(customer);
-			}
-
-			this.customers = customerService.getCustomers();
-		}
-	}
-
-	public List<Customer> getCustomers() {
-		return customers;
-	}
-
-	public void setCustomers(List<Customer> customers) {
-		this.customers = customers;
-	}
-
-	public List<Customer> getFilteredCustomerList() {
-		return filteredCustomerList;
-	}
-
-	public void setFilteredCustomerList(List<Customer> filteredCustomerList) {
-		this.filteredCustomerList = filteredCustomerList;
-	}
-
-	public List<Customer> getSelectedCustomerList() {
-		return selectedCustomerList;
-	}
-
-	public void setSelectedCustomerList(List<Customer> selectedCustomerList) {
-		this.selectedCustomerList = selectedCustomerList;
-	}
 }
